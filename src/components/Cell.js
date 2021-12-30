4dd26565687eb8077ef2beee6a6a715c16174a6d
bananasnowcones <43969773+bananasnowcones@users.noreply.github.com>
2021-12-30 01:49:38 -0800

    styling fixed

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { StyledCell } from './styles/StyledCell';
 import { TETRIMINOS } from '../tetriminos';
 
-const Cell = ({ type }) => (
-  <StyledCell type={type} color={TETRIMINOS[type].color} />
-)
+const Cell = ({type}) => (
+    <StyledCell type={type} color={TETRIMINOS[type].color}></StyledCell>
+);
 
 export default Cell;
--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -9,17 +9,15 @@ export const createStage = () =>
 export const checkCollision = (player, stage, { x: moveX, y: moveY }) => {
   for (let y = 0; y < player.tetrimino.length; y += 1) {
     for (let x = 0; x < player.tetrimino[y].length; x += 1) {
-      // 1. Check that we're on an actual tetrimino cell
+      // Check that we're on an actual tetrimino cell
       if (player.tetrimino[y][x] !== 0) {
         if (
-          // 2. Check that our move is inside the game areas height (y)
-          // We shouldn't go through the bottom of the play area
+          // 1. Check to see if we are within game area height
           !stage[y + player.pos.y + moveY] ||
-          // 3. Check that our move is inside the game areas width (x)
+          // 2. Check to see if we are within game area width
           !stage[y + player.pos.y + moveY][x + player.pos.x + moveX] ||
-          // 4. Check that the cell wer'e moving to isn't set to clear
-          stage[y + player.pos.y + moveY][x + player.pos.x + moveX][1] !==
-            'clear'
+          // 3. Check that the cell moving to is set to clear
+          stage[y + player.pos.y + moveY][x + player.pos.x + moveX][1] !== 'clear'
         ) {
           return true;
         }
